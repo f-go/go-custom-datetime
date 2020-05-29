5b6f3269e2a978cdeb6844d592b9682e80fddca2
Frank Göldner <frank.goeldner@myhotelshop.de>
2020-05-29 07:26:09 +0200

    added copyright information

--- custom_date_test.go
+++ custom_date_test.go
@@ -1,3 +1,7 @@
+// Copyright 2020 Frank Göldner. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package custom_datetime
 
 import (
